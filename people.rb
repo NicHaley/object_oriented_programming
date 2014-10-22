abe81ed241cb96a51e1b7b8f1f5dc8a6f5f661c8
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-10-22 09:47:37 -0400

    Finished problem

--- people.rb
+++ people.rb
@@ -1,12 +1,44 @@
-class Student
+module Greeting
+	def hello
+		puts "Hi, my name is #{@name}"
+	end
+end
+
+
+class Person
+	include Greeting
+
+	def initialize(name)
+		@name = name
+	end
+end
+
+
+class Student < Person
+
 	def learn
 		puts "I get it!"
 	end
 end
 
 
-class Instructor
+class Instructor < Person
+
 	def teach
 		puts "Everything in Ruby is an object"
 	end
 end
+
+instructor = Instructor.new("Chris")
+instructor.hello
+instructor.teach
+
+student = Student.new("Christina")
+student.hello
+student.learn
+student.teach
+
+#Both the student and instructor instances have access to the Module 'Greeting' by way of inheritance.
+#As such, they are able to use the method 'hello' and provide a greeting with their names
+#Both the student and instructor also have access to their respective instance methods, 'learn' and 'teach'.
+#Problems arise when they try to call eachothers instance methods, as in the problem above.
